dc780a7a5db51bf695ceb8a492a07580461fe312
Thnnnx <thanafonn@hotmail.com>
2020-12-13 04:08:28 +0700

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { from } from 'rxjs';
+import { Page2Module } from './shared/page2/page2/page2.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    Page2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
